8328264f2e721cf537f1351b20bc808050e52f35
CaTxDog <57478694+CaTxDog@users.noreply.github.com>
2024-03-17 20:38:32 +0600

    Задание 4
    Запуск тестов в разных браузерах

--- src/test/java/appmanager/ApplicationManager.java
+++ src/test/java/appmanager/ApplicationManager.java
@@ -2,7 +2,14 @@ package appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.Browser;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -16,9 +23,11 @@ public class ApplicationManager {
   private final Properties properties;
   WebDriver driver;
   WebDriverWait wait;
+  private String browser;
 
 
-  public ApplicationManager() {
+  public ApplicationManager(String browser) {
+        this.browser = browser;
         properties = new Properties();
   }
 
@@ -26,7 +35,20 @@ public class ApplicationManager {
 
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
-    driver = new ChromeDriver();
+
+    if (browser.equals(Browser.FIREFOX.browserName())) {
+      ProfilesIni profile = new ProfilesIni();
+      FirefoxProfile prof = profile.getProfile("fortest");
+      prof.setPreference("browser.download.dir","C:\\Developer\\selenium_for_qa");
+      prof.setPreference("browser.download.folderList", 2);
+      FirefoxOptions opt = new FirefoxOptions();
+      opt.setProfile(prof);
+      driver = new ChromeDriver();
+    } else if (browser.equals(Browser.CHROME.browserName())) {
+      driver = new ChromeDriver();
+    } else if (browser.equals(Browser.EDGE.browserName())){
+      driver = new EdgeDriver();
+    }
     driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
     driver.get(properties.getProperty("web.baseUrl"));
     sessionHelper = new SessionHelper(driver);
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,10 +3,11 @@ package tests;
 import appmanager.ApplicationManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.remote.Browser;
 
 public class TestBase {
 
-  public static final ApplicationManager app = new ApplicationManager();
+  public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.EDGE.browserName()));
 
 
   public void setUp() throws Exception {
